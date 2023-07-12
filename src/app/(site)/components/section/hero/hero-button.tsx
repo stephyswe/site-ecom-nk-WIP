db8e4f1562f8e2ab73f69fbf31cfd6dc3dd68864
Stephanie <stephaniiehallberg@gmail.com>
2023-07-12 12:21:02 +0200

    fix: datavalue

--- src/app/(site)/components/section/hero/hero-button.tsx
+++ src/app/(site)/components/section/hero/hero-button.tsx
@@ -2,13 +2,13 @@ export const HeroButton = ({ title }: any) => (
   <div className="hb y hc">
     <a
       id="EditorialButtonBlock"
-      datavalue="Omfamna-sommarens-alla-dofter|Dam"
+      data-value="Omfamna-sommarens-alla-dofter|Dam"
       className="a7 a8 o as at au an am b2 x dy t u hd fo fn he fr fq hf fu ft ai ah ag hg aw hh e8 e9 bj hi hj hk hl hm hn ew ho gm hp hq da z gw fk"
       href="file:///D:/kampanj/sommardoft/dam"
     >
       <span
         id="EditorialButtonBlock"
-        datavalue="Omfamna-sommarens-alla-dofter|Dam"
+        data-value="Omfamna-sommarens-alla-dofter|Dam"
       >
         {title}
       </span>
